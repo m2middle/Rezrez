5d2173e30662df7a55a49b77a8221164940d8160
DFRICHARD <54338939+DFRICHARD@users.noreply.github.com>
2023-02-02 17:57:40 +0100

    updated creditials db script

--- credentials_data.py
+++ credentials_data.py
@@ -3,11 +3,6 @@ import sqlite3
 conn = sqlite3.connect('users.db')
 cursor = conn.cursor()
 
-import sqlite3
-
-conn = sqlite3.connect('users.db')
-cursor = conn.cursor()
-
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY,
